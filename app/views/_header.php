d4b75b50f6063fb1211e12d5bde6bb9347057cb8
piotrnowak-pl <piotrnowak.pl@gmail.com>
2025-02-04 15:08:32 +0100

    Fix: Remove user also need remove data from subscription

--- app/views/_header.php
+++ app/views/_header.php
@@ -2,7 +2,7 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <title>Menor: Zadanie 2 i 3</title>
+    <title>Subskrypcja: Zadanie 2 i 3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
